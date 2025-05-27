aca9539100f5f483b568981a91cc3737549646fa
Gronglegrowth <fyredragonmagus@gmail.com>
2025-05-26 21:56:54 -0400

    1.21.1 neoforge port - removed fabric support - that isn't updated yet and probably will not be

--- src/main/java/com/gronglegrowth/vestis/Vestis.java
+++ src/main/java/com/gronglegrowth/vestis/Vestis.java
@@ -1,15 +1,14 @@
 package com.gronglegrowth.vestis;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod("vestis")
 public class Vestis
 {
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MODID = "vestis";
 
     public Vestis()
     {
